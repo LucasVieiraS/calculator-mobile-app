06ee23254ff93976808e8afc2ea269d029dd8015
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-09-29 08:53:57 -0300

    new: simple calculator

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,8 +8,60 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  result = 'Realize uma operação';
+  result: number | string = 0;
 
-  constructor() {}
+  value1;
+  value2;
 
+  constructor(private toastController: ToastController) {}
+
+  async notify(_message: string) {
+    const toast = await this.toastController.create({
+      message: _message,
+      duration: 3000
+    });
+
+    await toast.present();
+  }
+
+  canOperate() {
+    if (this.value1 == null || this.value2 == null) {
+      this.notify((this.value1 == null && 'Value 1 is null') ||
+      ( this.value2 == null && 'Value 2 is null') ||
+      (this.value1 == null && this.value2 == null) && 'Both are null');
+    }
+    return this.value1 != null || this.value2 != null;
+  }
+
+  // OPERATORS
+  sum() {
+    if (!this.canOperate()) { return; }
+    this.result = this.value1 + this.value2;
+  }
+
+  sub() {
+    if (!this.canOperate()) { return; }
+    this.result = this.value1 - this.value2;
+  }
+
+  mult() {
+    if (!this.canOperate()) { return; }
+    this.result = this.value1 * this.value2;
+  }
+
+  divi() {
+    if (!this.canOperate()) { return; }
+    this.result = this.value1 / this.value2;
+  }
+
+  // MORE/LESS THAN
+  mt() {
+    if (!this.canOperate()) { return; }
+    this.result = this.value1 > this.value2 && 'Verdade' || 'Falso';
+  }
+
+  lt() {
+    if (!this.canOperate()) { return; }
+    this.result = this.value1 < this.value2 && 'Verdade' || 'Falso';
+  }
 }
